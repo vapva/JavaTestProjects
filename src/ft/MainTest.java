f14154fb929abb94a47cab006ff850df4a670886
vapva <volodymyr.panchenko@gmail.com>
2015-12-20 23:52:15 +0200

    Initial 1

--- src/ft/MainTest.java
+++ src/ft/MainTest.java
@@ -1,27 +1,35 @@
 package ft;
 
 import java.io.File;
-import java.io.FilenameFilter;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
 public class MainTest {
 
-	public static void main(String[] args) throws URISyntaxException {
-		File workFile = new File(".." /*+ File.separator*//*+"FilesTest"*/);
-		File currentFile = new File(MainTest.class.getProtectionDomain().
-				getCodeSource().getLocation().toURI().getPath());
-		System.out.println(currentFile.getName());
-		System.out.println(workFile.getAbsolutePath());
-		System.out.println("isDirectory: "+ workFile.isDirectory());
+	public static void main(String[] args)  {
+		
+		String currentPath = null;
 		try {
-			System.out.println(workFile.getCanonicalPath());
-			File[] availableFiles = workFile.listFiles(new FilenameFilter() {
-				@Override
-				public boolean accept(File dir, String name) {
-					return true;
-				}
-			});
+			currentPath = MainTest.class.getProtectionDomain().
+					getCodeSource().getLocation().toURI().getPath()/*.getFile().intern()*/;
+		} catch (URISyntaxException e1) {
+			System.out.println("Wrong Path!");
+			System.exit(0);
+		}
+		if (!currentPath.contains("maintest.jar")) {
+			System.out.println("Application's file name is chaged, termination ...");
+			System.exit(0);
+		}
+
+		File workFile = new File("." /*+ File.separator*//*+"FilesTest"*/);
+
+		try {
+			System.out.println("Processed folder: " +workFile.getAbsolutePath());
+			System.out.println("CanonicalPath: " +workFile.getCanonicalPath());
+			System.out.println("isDirectory: "+ workFile.isDirectory());
+			System.out.println("CanonicalPath: " +workFile.getCanonicalPath());
+			System.out.println("-----------------------------");
+			File[] availableFiles = workFile.listFiles();
 			for (File file : availableFiles) {
 				System.out.println(file.getName());
 			}
@@ -29,8 +37,8 @@ public class MainTest {
 			e.printStackTrace();
 		}
 		System.out.println("-----------------------------");
-		String s=System.in.toString();
-		System.out.println(s);
+/*		String s=System.in.toString();
+		System.out.println(s);*/
 	}
 
 }
